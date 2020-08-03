80049a44be0b97d25324f22ab54f51234f334808
Erica Girges <ericafiorenza@gmail.com>
2020-08-03 14:57:36 -0700

    contact section

--- js/index.js
+++ js/index.js
@@ -46,46 +46,41 @@ logo.setAttribute('src', siteContent["nav"]["img-src"]);
 const nav = document.querySelector("nav");
 
 const navItem1 = nav.querySelector("a");
-navItem1.textContent = "Services";
-navItem1.classList.add("nav-item-1");
+navItem1.textContent = siteContent["nav"]["nav-item-1"];
 
 const navItem2 =navItem1.nextElementSibling;
-navItem2.textContent = "Product";
-navItem2.classList.add("nav-item-2");
+navItem2.textContent = siteContent["nav"]["nav-item-2"];
 
 const navItem3 = navItem2.nextElementSibling;
-navItem3.textContent = "Vision";
-navItem3.classList.add("nav-item-3");
+navItem3.textContent = siteContent["nav"]["nav-item-3"];
 
 const navItem4 = navItem3.nextElementSibling;
-navItem4.textContent = "Features";
-navItem4.classList.add("nav-item-4");
+navItem4.textContent = siteContent["nav"]["nav-item-4"];
 
 const navItem5 = navItem4.nextElementSibling;
-navItem5.textContent = "About";
-navItem5.classList.add("nav-item-5");
+navItem5.textContent = siteContent["nav"]["nav-item-5"];
 
 const navItem6 = navItem5.nextElementSibling;
-navItem6.textContent = "Contact";
-navItem6.classList.add("nav-item-6");
+navItem6.textContent = siteContent["nav"]["nav-item-6"];
 
 
 // CTA H1 and Image
 const ctaText = document.querySelector(".cta-text");
 
 const mainHeading = ctaText.querySelector("h1");
-mainHeading.textContent = "DOM Is Awesome";
+const mainHeadingText = siteContent["cta"]["h1"]
+mainHeading.innerHTML = mainHeadingText.split(' ').join("<br />");
 
 const button = ctaText.querySelector("button");
-button.textContent = "Get Started";
+button.textContent = siteContent["cta"]["button"];
 
 const cta = document.querySelector(".cta");
 
 const imgCodeSnip = cta.querySelector("img");
-imgCodeSnip.src = "img/header-img.png";
+imgCodeSnip.src = siteContent["cta"]["img-src"];
 
 
-// .text-content and Image
+// main-content and Image
 const allContent = document.querySelectorAll(".text-content");
 
 function updateInfo(h4Text, pText, i){
@@ -95,13 +90,27 @@ function updateInfo(h4Text, pText, i){
   newInfo.textContent = pText;
 }
 
-updateInfo("Features", "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.", 0);
+updateInfo(siteContent["main-content"]["features-h4"], siteContent["main-content"]["features-content"], 0);
 
-updateInfo("About", "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.", 1);
+updateInfo(siteContent["main-content"]["about-h4"], siteContent["main-content"]["about-content"], 1);
 
-updateInfo("Services", "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.", 2);
+updateInfo(siteContent["main-content"]["services-h4"], siteContent["main-content"]["services-content"], 2);
 
-updateInfo("Product", "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.", 3);
+updateInfo(siteContent["main-content"]["product-h4"], siteContent["main-content"]["product-content"], 3);
+
+updateInfo(siteContent["main-content"]["vision-h4"], siteContent["main-content"]["vision-content"], 4)
 
 const middleImg = document.getElementById("middle-img");
-middleImg.src = "img/mid-page-accent.jpg";
+middleImg.src = siteContent["main-content"]["middle-img-src"];
+
+
+// contact
+const contact = document.querySelector(".contact");
+const contactH4 = contact.querySelector("h4");
+contactH4.textContent = siteContent["contact"]["contact-h4"];
+
+const contactTexts = contact.querySelectorAll("p");
+contactTexts[0].textContent = siteContent["contact"]["address"];
+contactTexts[1].textContent = siteContent["contact"]["phone"];
+contactTexts[2].textContent = siteContent["contact"]["email"];
+
